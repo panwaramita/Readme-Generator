86f84d2111e391af1565830b990abd155867a7c5
Amita <panwar.amita@gmail.com>
2021-01-26 23:42:15 -0500

    updated the js file

---
+++ Read.md
@@ -0,0 +1,32 @@
+# a
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+ ## Table of Contents
+
+* [Project Description](#Project Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Tests](#Tests)
+ * [License](#License)
+* [Contributions](#Contributions)
+* [Questions](#Questions)
+
+        ## Project Description
+        a 
+
+        #Installation
+        a 
+
+        # Usage
+        a
+        
+        # Tests
+        a
+        
+        # Contributions
+        undefined
+
+        # Questions
+        For more question , please contract project owner via email listed below.
+        Name: a:https://github.com/$(data.name)
+        Mail: a
+  
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown=require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =()=>inquirer.prompt([
+    {
+        type: 'input',
+      name: 'title',
+      message: 'Enter the title of the project?',
+    },
+    {
+        type: 'input',
+      name: 'description',
+      message: 'Enter the description of the project?',
+    },
+    {
+        type: 'input',
+      name: 'installation',
+      message: 'Enter all the Installation  required for the project?',
+    },
+    
+    {
+        type: 'input',
+      name: 'usage',
+      message: 'Enter the Usage of the project?',
+    },
+    {
+        type: 'list',
+      name: 'license',
+      message: 'Select the License for the project?',
+    choices:['Apache 2.0 License','GNU GPL v3']
+    },
+    {
+        type: 'input',
+      name: 'contributing',
+      message: 'Enter the contributors for the project?',
+    },
+    {
+        type: 'input',
+      name: 'tests',
+      message: 'Enter the tests for the project?',
+    },
+    {
+        type: 'input',
+      name: 'name',
+      message: 'Enter the github username?',
+    },
+    {
+        type: 'input',
+      name: 'email',
+      message: 'Enter the email?',
+    }
+]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions().then((answers) => {
+      try {
+        const read = generateMarkdown(answers);
+        fs.writeFileSync('Read.md', read);
+        console.log('Successfully wrote to index.html');
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
 
 // Function call to initialize app
 init();
